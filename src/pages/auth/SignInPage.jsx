44c4158252065254852101262931e4ee621568eb
Vi Huynh <huynhtrieutrivi@gmail.com>
2024-09-21 23:19:45 -0700

    feat(user): validation, simple signup & login, store info on database, set base folder to src, new dev dependency (#4)
    - User re-direct to login page after sign-up using react-dom-router - useNavigate and consume state on sign-in page using useLocation.
    - Installed new dependency (vite-jsconfig-paths) to point to src as base path for convience.
    - Implemented createSession service.
    - Removed success message from sign-up page.

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,6 +1,8 @@
+import { createSession } from "services/user";
 import AuthForm from "./AuthForm/authIndex";
 import FormContainer from "./FormContainer";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const SIGN_IN_FIELDS = [
     { label: "username", type: "text" },
@@ -8,11 +10,38 @@ const SIGN_IN_FIELDS = [
 ];
 
 const SignInPage = () => {
+    const [error, setError] = useState("");
+    const location = useLocation();
     return (
         <FormContainer>
-            <AuthForm fields={SIGN_IN_FIELDS} submitButtonText="sign in" />
+            {error && (
+                <p className="text-red-500 bg-red-200 px-3 py-2 rounded">
+                    {error}
+                </p>
+            )}
+            {location.state && location.state.accountCreated && (
+                <p className="text-green-500 bg-green-200 px-3 py-2 m-2 rounded">
+                    Account has been created successfully. Please sign in.
+                </p>
+            )}
+            <AuthForm
+                fields={SIGN_IN_FIELDS}
+                submitButtonText="sign in"
+                onSubmit={async (values) => {
+                    const response = await createSession({
+                        username: values.username,
+                        password: values.password,
+                    });
+                    if (response.status == 201) {
+                        console.log("log in success!");
+                        setError("");
+                    } else {
+                        setError((await response.json()).error);
+                    }
+                }}
+            />
             <Link to="/sign-up" className="text-green-800 hover:underline">
-                create an account
+                Create an Account
             </Link>
         </FormContainer>
     );
--- src/services/user.js
+++ src/services/user.js
@@ -3,3 +3,7 @@ import apiFetch from "./apiFetch";
 export const createUser = ({ username, password }) => {
     return apiFetch("POST", "/users", { username, password });
 };
+
+export const createSession = ({ username, password }) => {
+    return apiFetch("POST", "/users/session", { username, password });
+};
